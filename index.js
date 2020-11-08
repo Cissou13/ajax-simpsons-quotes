9981190d0d11d1fc60eebcaaaf44b834aec13a04
Cyrille F <cyrille.fabre@gmail.com>
2020-11-08 22:55:42 +0100

    Printing one single quote at a time

---
+++ index.js
@@ -0,0 +1,24 @@
+// This function loads pokemon data from the Pokemon API
+function getSimpsonQuote() {
+  // Feel free to download this HTML and edit it, to use another Pokemon ID
+  // const pokemonId = 1;
+  const url = `https://simpsons-quotes-api.herokuapp.com/quotes`;
+  axios
+    .get(url)
+    .then(function (response) {
+      return response.data; // response.data instead of response.json() with fetch
+    })
+    .then(function (simpsonQuote) {
+      console.log('data decoded from JSON:', simpsonQuote);
+
+      // Build a block of HTML
+      const simpsonQuoteHtml = `
+        <p><strong>${simpsonQuote[0].character}</strong></p>
+        <img src="${simpsonQuote[0].image}" />
+      <blockquote>${simpsonQuote[0].quote}</blockquote>`;
+
+      document.querySelector('#simpson-quote').innerHTML = simpsonQuoteHtml;
+    });
+}
+
+getSimpsonQuote();
